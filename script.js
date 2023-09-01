9ba34a869a69f76840b94bbd6b556f5864437c21
TanimShahriar <ssmtsshr@gmail.com>
2023-09-01 12:20:13 +0600

    all part completed without short by view button

--- script.js
+++ script.js
@@ -40,6 +40,8 @@ const handleCardVideo = async (cardId) => {
   cardContainer.textContent = '';
 
 
+
+
   if (cardData.length === 0) {
     const noData = document.createElement('div');
     noData.innerHTML = `
@@ -59,37 +61,47 @@ const handleCardVideo = async (cardId) => {
     return;
   }
 
+
   cardData.forEach((video) => {
     console.log(video);
     const div = document.createElement('div');
     div.innerHTML = `
-    <div class="max-w-sm mx-auto bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <a class="relative" href="#">
-      <img class="rounded-t-lg h-60 w-full" src="${video.thumbnail}" />
-      <p class=" text-white ml-72 absolute bottom-6">${video.others.posted_date}</p>
-    </a>
+    <div class="max-w-sm mx-auto  bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div class="relative flex">
+    <img class="rounded-t-lg h-60 w-full" src="${video.thumbnail}" />
+    <p class="ml-56 absolute bottom-6">
+      ${video.others?.posted_date ? (
+        `<button class="bg-[#171717]  text-white rounded-sm text-xs px-1 py-1 ">
+          ${formatTime(video.others.posted_date)}
+        </button>`
+      ) : ""}
+    </p>
+  </div>
+  
     <div class="p-5">
-      <div class="flex flex-start gap-4 " >
-      <img class="h-10 w-10 rounded-full" src="${video.authors[0].profile_picture}">
-      <a href="#">
-        <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">${video.title}</h5>
-      </a>
-      </div>
-      <div class="flex justify-between">
-      <p class="mb-3 ml-14 font-normal text-gray-700 dark:text-gray-400">${video.authors[0].profile_name}</p>
-      <p class="mb-3 ml-14 font-normal text-gray-700 dark:text-gray-400">${video.authors[0]?.verified ? (`<img src="img/verified.svg">`) : ""}</p>
+          <div class="flex flex-start gap-4 " >
+          <img class="h-10 w-10 rounded-full" src="${video.authors[0].profile_picture}">
+          <a href="#">
+          <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">${video.title}</h5>
+          </a>
+          </div>
+          <div class="flex justify-between">
+          <p class="mb-3 ml-14 font-normal text-gray-700 dark:text-gray-400">${video.authors[0].profile_name}</p>
+          <p class="mb-3 ml-14 font-normal text-gray-700 dark:text-gray-400">${video.authors[0]?.verified ? (`<img src="img/verified.svg">`) : ""}</p>
       
-      </div >
+    </div >
 
 
-  <p class="ml-14">${video.others.views}</p>
+  <p class="ml-14">${video.others.views} views</p>
     </div >
   </div >
   `;
     cardContainer.appendChild(div);
   })
+
 }
 
+//Default videos
 handleCardVideo('1000')
 
 
@@ -100,4 +112,16 @@ blogButton.addEventListener('click', () => {
   const blogUrl = 'blog.html';
   window.open(blogUrl, '_blank');
 });
+
+function formatTime(seconds) {
+  const minutes = Math.floor(seconds / 3600);
+  const remainingSeconds = seconds % 60;
+
+  return `${minutes} Hours ${remainingSeconds} min ago`;
+}
+
+
+//short by view
+
+
 
