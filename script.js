751939198cd374462a8967cef7606f7133a7c440
TanimShahriar <ssmtsshr@gmail.com>
2023-09-01 15:16:46 +0600

    Finalizing the assignment

--- script.js
+++ script.js
@@ -32,14 +32,12 @@ const handleCardVideo = async (cardId) => {
   console.log(cardId);
   const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${cardId}`);
   const data = await response.json();
-  console.log(data);
+  // console.log(data);
   const cardData = data.data;
   // console.log(cardData);
 
   const cardContainer = document.getElementById('cardContainer');
   cardContainer.textContent = '';
-
-
 
 
   if (cardData.length === 0) {
@@ -63,7 +61,7 @@ const handleCardVideo = async (cardId) => {
 
 
   cardData.forEach((video) => {
-    console.log(video);
+    // console.log(video);
     const div = document.createElement('div');
     div.innerHTML = `
     <div class="max-w-sm mx-auto  bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
@@ -113,6 +111,8 @@ blogButton.addEventListener('click', () => {
   window.open(blogUrl, '_blank');
 });
 
+
+//function for converting seconds to hours and minutes
 function formatTime(seconds) {
   const minutes = Math.floor(seconds / 3600);
   const remainingSeconds = seconds % 60;
@@ -121,7 +121,56 @@ function formatTime(seconds) {
 }
 
 
-//short by view
+//shortByView function
+
+const handleSortByView = async () => {
+  const response = await fetch("https://openapi.programming-hero.com/api/videos/category/1000");
+  const data = await response.json();
+  const viewData = data.data;
 
 
+  viewData.sort((a, b) => parseInt(b.others.views, 10) - parseInt(a.others.views, 10));
 
+
+  const cardContainer = document.getElementById('cardContainer');
+  cardContainer.textContent = '';
+
+
+  viewData.forEach((video) => {
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <div class="max-w-sm mx-auto  bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div class="relative flex">
+    <img class="rounded-t-lg h-60 w-full" src="${video.thumbnail}" />
+    <p class="ml-56 absolute bottom-6">
+      ${video.others?.posted_date ? (
+        `<button class="bg-[#171717]  text-white rounded-sm text-xs px-1 py-1 ">
+          ${formatTime(video.others.posted_date)}
+        </button>`
+      ) : ""}
+    </p>
+  </div>
+  
+    <div class="p-5">
+          <div class="flex flex-start gap-4 " >
+          <img class="h-10 w-10 rounded-full" src="${video.authors[0].profile_picture}">
+          <a href="#">
+          <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">${video.title}</h5>
+          </a>
+          </div>
+          <div class="flex justify-between">
+          <p class="mb-3 ml-14 font-normal text-gray-700 dark:text-gray-400">${video.authors[0].profile_name}</p>
+          <p class="mb-3 ml-14 font-normal text-gray-700 dark:text-gray-400">${video.authors[0]?.verified ? (`<img src="img/verified.svg">`) : ""}</p>
+      
+    </div >
+
+
+  <p class="ml-14">${video.others.views} views</p>
+    </div >
+  </div > 
+
+
+    `;
+    cardContainer.appendChild(div);
+  });
+}
